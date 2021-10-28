df5679a8b6e830dac2dd55efa163cf24c512d62b
Duskofdawn <ianperry95@gmail.com>
2021-10-27 20:16:05 -0500

    Updated name and icons

--- view/header2.php
+++ view/header2.php
@@ -3,7 +3,7 @@
         <header class="sticky-top shadow">
             <nav class="navbar navbar-expand-lg navbar-light color-bg">
             <div class="container">
-                <a class="navbar-brand" href="/index.php"><img src="img/cartalog-text.png" alt="cartalog logo" style="max-width: 23%"></img> </a>
+                <a class="navbar-brand" href="/index.php"><img src="img/cartana.png" alt="cartalog logo" style="max-width: 23%"></img> </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
